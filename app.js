5d8dfe8b9d8f842fffd692b61ac0a5c2e50076b6
Present_Jay.Dev <Presentj94@gmail.com>
2022-09-22 16:42:37 +0900

    chore: 코드 정리

--- app.js
+++ app.js
@@ -1,44 +1,38 @@
 const subredditName = "programmerhumor";
-const category = "topic"
+const fetchingAmount=100;
 
 async function fetchData() {
+    const response = await fetch(`https://www.reddit.com/r/programmerhumor.json?limit=${fetchingAmount}`);
+    const body = await response.json();
 
-  const response = await fetch(`https://www.reddit.com/r/programmerhumor.json`);
-  const body = await response.json();
+    let displaySubredditName = document.getElementById("subreddit_name")
 
-  let displayCategory = document.getElementById("category_text");
-  let displaySubredditName = document.getElementById("subreddit_name")
+    var container = document.getElementById("container");
 
-  var container = document.getElementById("container");
-
-  try {
-    let parentdiv = document.createElement("div");
-    let targetlist=body.data.children
-    let index = getRandomInt(0, targetlist.length)
-
-    if (body.data.children[index].data.post_hint === "image") {
-  
+    try {
+        let parentdiv = document.createElement("div");
+        let index=0
+        while (body.data.children[index].data.post_hint != "image") {
+            index = getRandomInt(0, 100)
+        }
+    
         let div = document.createElement("div");
         let h4 = document.createElement("h4");
         let image = document.createElement("img");
-  
+
         image.src = body.data.children[index].data.url;
         h4.textContent = body.data.children[index].data.title;
-        displayCategory.textContent = `Category: ${category}`;
         displaySubredditName.textContent = `Subreddit: ${subredditName}`;
         
         div.appendChild(h4);
         div.appendChild(image);
         parentdiv.appendChild(div);
+        container.appendChild(parentdiv);
+
+    } catch (e) {
+        console.log(`Error: ${e}`)
     }
-
-    container.appendChild(parentdiv);
-
-  } catch (e) {
-    console.log(`Error: ${e}`)
-  }
 }
-
 
 function getRandomInt(min, max) {
     max = Math.floor(max);
