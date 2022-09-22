01b773cfb79e987b74f48ac6b0c508f6974c5e5f
Present_Jay.Dev <Presentj94@gmail.com>
2022-09-22 16:55:48 +0900

    feat: img DOM 정리, secret 적용

--- app.js
+++ app.js
@@ -1,34 +1,17 @@
-const subredditName = "programmerhumor";
-const fetchingAmount=100;
+const subredditName = process.env.SUBREDDIT_NAME;
+const fetchingAmount = process.env.AMOUNT;
 
 async function fetchData() {
-    const response = await fetch(`https://www.reddit.com/r/programmerhumor.json?limit=${fetchingAmount}`);
+    const response = await fetch(`https://www.reddit.com/r/${subredditName}.json?limit=${fetchingAmount}`);
     const body = await response.json();
-
-    let displaySubredditName = document.getElementById("subreddit_name")
-
-    var container = document.getElementById("container");
-
     try {
-        let parentdiv = document.createElement("div");
         let index=0
-        while (body.data.children[index].data.post_hint != "image") {
+        while (body.data.children[index].data.post_hint != "image")
             index = getRandomInt(0, 100)
-        }
-    
-        let div = document.createElement("div");
-        let h4 = document.createElement("h4");
         let image = document.createElement("img");
-
         image.src = body.data.children[index].data.url;
-        h4.textContent = body.data.children[index].data.title;
-        displaySubredditName.textContent = `Subreddit: ${subredditName}`;
-        
-        div.appendChild(h4);
-        div.appendChild(image);
-        parentdiv.appendChild(div);
-        container.appendChild(parentdiv);
-
+        image.style = "display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;"
+        document.body.appendChild(image);        
     } catch (e) {
         console.log(`Error: ${e}`)
     }
